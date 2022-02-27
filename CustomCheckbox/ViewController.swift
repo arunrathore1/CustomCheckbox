f1cd8140a33c26539e6ff3cfa7668f8a1f20495b
Arun Rathore <arunrathore0821@gmail.com>
2022-02-27 09:02:53 +0530

    Initial Commit

---
+++ CustomCheckbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCheckbox
+//
+//  Created by Arun Rathore on 27/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
